77446fa78e062e105db3fd17f2eed4c0bd03130c
Pawel Wilk <pawel@wilk.pro>
2025-04-15 21:32:35 +0200

    change commands

--- cmd/exporter/main.go
+++ cmd/exporter/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"path/filepath"
+
+	"github.com/mephir/teryt-golang/internal/teryt"
+)
+
 func main() {
-	// This is an empty main function
+	basePath, err := filepath.Abs("_zrzuty/")
+	if err != nil {
+		panic(err)
+	}
+
+	paths := []string{
+		filepath.Join(basePath, "TERC_Adresowy_2025-04-01.xml"),
+		filepath.Join(basePath, "WMRODZ_2025-04-01.xml"),
+		filepath.Join(basePath, "SIMC_Adresowy_2025-04-01.xml"),
+		filepath.Join(basePath, "ULIC_Adresowy_2025-04-01.xml"),
+	}
+
+	teryt := teryt.NewInstance()
+	teryt.LoadFromFiles(paths...)
+
+	// writer := writer.NewWriter("gob", nil)
+	// writer.Write(teryt, "teryt.gob")
 }
--- cmd/stat/main.go
+++ cmd/stat/main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/mephir/teryt-golang/internal/teryt"
 )
 
+type inputFiles []string
+
+var output string
+var input inputFiles
+
+func (i *inputFiles) Set(value string) error {
+	if value == "" {
+		return fmt.Errorf("input file cannot be empty")
+	}
+
+	if _, err := os.Stat(value); err != nil {
+		return err
+	}
+
+	*i = append(*i, value)
+	return nil
+}
+
+func (i *inputFiles) String() string {
+	return fmt.Sprint(*i)
+}
+
+func init() {
+	flag.StringVar(&output, "output", "", "Output zstd training data file")
+	flag.Var(&input, "input", "Input XML files to process (can be specified multiple times)")
+}
+
 func main() {
+	flag.Parse()
+
 	basePath, err := filepath.Abs("_zrzuty/")
 	if err != nil {
 		panic(err)
